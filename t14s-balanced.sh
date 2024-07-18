f35225d4fcf07b47f65dd2299d07311a19f4deda
Mario <75674245+Tuxino88@users.noreply.github.com>
2024-07-18 11:50:54 +0200

    Update t14s-balanced.sh

--- t14s-balanced.sh
+++ t14s-balanced.sh
@@ -21,7 +21,6 @@ echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog
 echo balanced | sudo tee /sys/firmware/acpi/platform_profile 
 
 #PM
-echo 0 | sudo tee /sys/module/snd_hda_intel/parameters/power_save
 echo on | sudo tee /sys/bus/i2c/devices/i2c-5/device/power/control
 echo on | sudo tee /sys/bus/i2c/devices/i2c-7/device/power/control
 echo on | sudo tee /sys/bus/i2c/devices/i2c-9/device/power/control
