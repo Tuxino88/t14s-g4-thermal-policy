13bb09b0dff2485c1060ddb418c7aec140a2c2a1
Mario <75674245+Tuxino88@users.noreply.github.com>
2024-07-19 10:15:42 +0200

    Update t14s-superbat.sh

--- t14s-superbat.sh
+++ t14s-superbat.sh
@@ -21,37 +21,37 @@ echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog
 echo low-power | sudo tee /sys/firmware/acpi/platform_profile 
 
 #PM
-echo off | sudo tee /sys/bus/i2c/devices/i2c-5/device/power/control
-echo off | sudo tee /sys/bus/i2c/devices/i2c-7/device/power/control
-echo off | sudo tee /sys/bus/i2c/devices/i2c-9/device/power/control
-echo off | sudo tee /sys/bus/i2c/devices/i2c-2/device/power/control
-echo off | sudo tee /sys/bus/i2c/devices/i2c-4/device/power/control
-echo off | sudo tee /sys/bus/i2c/devices/i2c-15/device/power/control
-echo off | sudo tee /sys/bus/i2c/devices/i2c-6/device/power/control
-echo off | sudo tee /sys/bus/i2c/devices/i2c-8/device/power/control
-echo off | sudo tee /sys/bus/i2c/devices/i2c-1/device/power/control
-echo off | sudo tee /sys/bus/i2c/devices/i2c-3/device/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:08.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:0d.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:1f.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:07.2/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:04.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:14.3/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:0d.3/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:07.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:1f.5/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:1f.3/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:15.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:06.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:16.3/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:14.2/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:04:00.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:02.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:14.0/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:1f.4/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:0d.2/power/control
-echo off | sudo tee /sys/bus/pci/devices/0000:00:00.0/power/control
-echo off | sudo tee /sys/block/nvme0n1/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-5/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-7/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-9/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-2/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-4/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-15/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-6/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-8/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-1/device/power/control
+echo auto | sudo tee /sys/bus/i2c/devices/i2c-3/device/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:08.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:0d.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:1f.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:07.2/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:04.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:14.3/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:0d.3/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:07.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:1f.5/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:1f.3/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:15.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:06.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:16.3/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:14.2/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:04:00.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:02.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:14.0/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:1f.4/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:0d.2/power/control
+echo auto | sudo tee /sys/bus/pci/devices/0000:00:00.0/power/control
+echo auto | sudo tee /sys/block/nvme0n1/device/power/control
 
 #AHCI
 echo disabled | sudo tee /sys/class/net/wlp0s20f3/device/power/wakeup
@@ -67,4 +67,4 @@ echo disabled | sudo tee /sys/bus/usb/devices/3-3/power/wakeup
 #ASPM
 echo powersupersave | sudo tee /sys/module/pcie_aspm/parameters/policy
 echo 1 | sudo tee /sys/module/snd_hda_intel/parameters/power_save
-#echo 1500 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
+echo 1500 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
