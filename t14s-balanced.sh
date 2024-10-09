3bab39a07cdc516a897ae6bd68aca23784999a5f
Mario <75674245+Tuxino88@users.noreply.github.com>
2024-10-09 07:47:25 +0200

    Update t14s-balanced.sh
    update policy

--- t14s-balanced.sh
+++ t14s-balanced.sh
@@ -10,7 +10,7 @@ echo 45000000 | sudo tee /sys/class/powercap/intel-rapl-mmio:0/constraint_1_powe
 #echo 2440 | sudo tee /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_1_time_window_us
 
 #CPU
-echo powersave | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo balance_performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 echo 8 | sudo tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
 echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/min_perf_pct
@@ -19,6 +19,10 @@ echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/hwp_dynamic_boost
 echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog
 echo balanced | sudo tee /sys/firmware/acpi/platform_profile 
+
+#GPU
+echo 1300 | sudo tee /sys/class/drm/card1/gt_max_freq_mhz
+echo 1300 | sudo tee /sys/class/drm/card1/gt_boost_freq_mhz
 
 #PM
 echo on | sudo tee /sys/bus/i2c/devices/i2c-5/device/power/control
@@ -54,17 +58,17 @@ echo on | sudo tee /sys/bus/pci/devices/0000:00:00.0/power/control
 echo on | sudo tee /sys/block/nvme0n1/device/power/control
 
 #AHCI
-echo disabled | sudo tee /sys/class/net/wlp0s20f3/device/power/wakeup
-echo disabled | sudo tee /sys/bus/usb/devices/usb3/power/wakeup
-echo disabled | sudo tee /sys/bus/usb/devices/usb1/power/wakeup
-echo disabled | sudo tee /sys/bus/usb/devices/3-2/power/wakeup
-echo disabled | sudo tee /sys/bus/usb/devices/3-9/power/wakeup
-echo disabled | sudo tee /sys/bus/usb/devices/usb4/power/wakeup
-echo disabled | sudo tee /sys/bus/usb/devices/3-10/power/wakeup
-echo disabled | sudo tee /sys/bus/usb/devices/usb2/power/wakeup
-echo disabled | sudo tee /sys/bus/usb/devices/3-3/power/wakeup
+echo enabled | sudo tee /sys/class/net/wlp0s20f3/device/power/wakeup
+echo enabled | sudo tee /sys/bus/usb/devices/usb3/power/wakeup
+echo enabled | sudo tee /sys/bus/usb/devices/usb1/power/wakeup
+echo enabled | sudo tee /sys/bus/usb/devices/3-2/power/wakeup
+echo enabled | sudo tee /sys/bus/usb/devices/3-9/power/wakeup
+echo enabled | sudo tee /sys/bus/usb/devices/usb4/power/wakeup
+echo enabled | sudo tee /sys/bus/usb/devices/3-10/power/wakeup
+echo enabled | sudo tee /sys/bus/usb/devices/usb2/power/wakeup
+echo enabled | sudo tee /sys/bus/usb/devices/3-3/power/wakeup
 
 #ASPM
-echo default | sudo tee /sys/module/pcie_aspm/parameters/policy
+echo powersave | sudo tee /sys/module/pcie_aspm/parameters/policy
 echo 0 | sudo tee /sys/module/snd_hda_intel/parameters/power_save
-echo 500 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
+#echo 6000 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
