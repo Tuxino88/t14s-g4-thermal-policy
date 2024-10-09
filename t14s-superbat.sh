ee232a4cb7aa3db00566d43a8bcb1e04a579d690
Mario <75674245+Tuxino88@users.noreply.github.com>
2024-10-09 07:52:21 +0200

    Update t14s-superbat.sh
    update policy

--- t14s-superbat.sh
+++ t14s-superbat.sh
@@ -2,23 +2,27 @@
 
 #cTDP
 echo 12000000 | sudo tee /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_power_limit_uw
-echo 12000000 | sudo tee /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_1_power_limit_uw
-echo 12000000 | sudo tee /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_2_power_limit_uw
+echo 15000000 | sudo tee /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_1_power_limit_uw
+echo 20000000 | sudo tee /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_2_power_limit_uw
 echo 12000000 | sudo tee /sys/class/powercap/intel-rapl-mmio:0/constraint_0_power_limit_uw
-echo 12000000 | sudo tee /sys/class/powercap/intel-rapl-mmio:0/constraint_1_power_limit_uw
+echo 15000000 | sudo tee /sys/class/powercap/intel-rapl-mmio:0/constraint_1_power_limit_uw
 #echo 127926272 | sudo tee /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_0_time_window_us
 #echo 2440 | sudo tee /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_1_time_window_us
 
 #CPU
-echo powersave | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
+echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
 echo power | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 echo 15 | sudo tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
 echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/min_perf_pct
-echo 10 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
+echo 15 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
 echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/hwp_dynamic_boost
 echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog
 echo low-power | sudo tee /sys/firmware/acpi/platform_profile 
+
+#GPU
+echo 500 | sudo tee /sys/class/drm/card1/gt_max_freq_mhz
+echo 500 | sudo tee /sys/class/drm/card1/gt_boost_freq_mhz
 
 #PM
 echo auto | sudo tee /sys/bus/i2c/devices/i2c-5/device/power/control
@@ -67,4 +71,4 @@ echo disabled | sudo tee /sys/bus/usb/devices/3-3/power/wakeup
 #ASPM
 echo powersupersave | sudo tee /sys/module/pcie_aspm/parameters/policy
 echo 1 | sudo tee /sys/module/snd_hda_intel/parameters/power_save
-echo 1500 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
+#echo 1500 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
