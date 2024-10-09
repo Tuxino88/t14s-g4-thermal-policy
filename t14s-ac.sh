a15fed3b2376cf25223ae3c2cc46def0b7612aec
Mario <75674245+Tuxino88@users.noreply.github.com>
2024-10-09 07:45:03 +0200

    Update t14s-ac.sh

--- t14s-ac.sh
+++ t14s-ac.sh
@@ -10,16 +10,19 @@ echo 55000000 | sudo tee /sys/class/powercap/intel-rapl-mmio:0/constraint_1_powe
 #echo 2440 | sudo tee /sys/class/powercap/intel-rapl/intel-rapl:0/constraint_1_time_window_us
 
 #CPU
-echo powersave | sudo tee /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo balance_performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
+echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference
 echo 4 | sudo tee /sys/devices/system/cpu/cpu*/power/energy_perf_bias
 echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/min_perf_pct
 echo 100 | sudo tee /sys/devices/system/cpu/intel_pstate/max_perf_pct
 echo 0 | sudo tee /sys/devices/system/cpu/intel_pstate/no_turbo
 echo 1 | sudo tee /sys/devices/system/cpu/intel_pstate/hwp_dynamic_boost
 echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog
-echo balanced | sudo tee /sys/firmware/acpi/platform_profile 
+echo performance | sudo tee /sys/firmware/acpi/platform_profile 
 
+#GPU
+echo 1300 | sudo tee /sys/class/drm/card1/gt_max_freq_mhz
+echo 1300 | sudo tee /sys/class/drm/card1/gt_boost_freq_mhz
 
 #PM
 echo on | sudo tee /sys/bus/i2c/devices/i2c-5/device/power/control
@@ -66,6 +69,6 @@ echo disabled | sudo tee /sys/bus/usb/devices/usb2/power/wakeup
 echo disabled | sudo tee /sys/bus/usb/devices/3-3/power/wakeup
 
 #ASPM
-echo default | sudo tee /sys/module/pcie_aspm/parameters/policy
+echo performance | sudo tee /sys/module/pcie_aspm/parameters/policy
 echo 0 | sudo tee /sys/module/snd_hda_intel/parameters/power_save
-echo 500 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
+#echo 6000 | sudo tee /proc/sys/vm/dirty_writeback_centisecs
